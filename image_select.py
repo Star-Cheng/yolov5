17705de7584338543d639b3e2e7e85e180fedac4
Star-Cheng <1335929010@qq.com>
2025-04-11 22:11:01 +0800

    yolov5-attention初始化

---
+++ image_select.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+
+def image_select():
+    iamge_ad = input("请输入选择路径：")
+    if iamge_ad == str(0):
+        quit()
+    tar_ad = input("请输入保存路径：")
+    num = 5
+    image_files = os.listdir(iamge_ad)
+    selected_im = []
+    j = 0
+    for i in image_files:
+        selected_im.append(i)
+        if j < len(selected_im):
+            if not os.path.exists(tar_ad):
+                os.makedirs(tar_ad)
+                shutil.copy(os.path.join(iamge_ad, selected_im[j]), tar_ad)
+            else:
+                shutil.copy(os.path.join(iamge_ad, selected_im[j]), tar_ad)
+            j += num
+
+
+image_select()
+print('end')
+
